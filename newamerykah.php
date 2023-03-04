35074b5a60f2d3589e0a3693896326c6642ef2c2
happychildren <vojja01@gmail.com>
2023-03-04 07:53:13 +0100

    sfv

--- newamerykah.php
+++ newamerykah.php
@@ -1,3 +1,24 @@
 <?php
-
+/** 
+ * New Amerykah?
+ * 
+ * @package New amerykah 
+ * @author Your Name
+ * @copyright 2020 Your Name 
+ * @license GPL-2.0-or-later 
+ * 
+ * @wordpress-plugin 
+ * Plugin Name: New Amerykah
+ * Plugin URI: https://mysite.com/hello-world 
+ * Description: Cao
+ * Version: 0.0.1 
+ * Author: Your Name 
+ * Author URI: https://mysite.com 
+ * Text Domain: newamerykah
+ * License: GPL v2 or later 
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * GitHub Plugin URI: 10000thmonkey/newamerykah
+ * GitHub Plugin URI: https://github.com/10000thmonkey/newamerykah
+ * 
+ * */
 ?>
