6afc3e631b917e700037179bca2fd1d8ccc0c810
happychildren <vojja01@gmail.com>
2023-03-04 08:37:10 +0100

    svoih

--- newamerykah.php
+++ newamerykah.php
@@ -125,7 +125,7 @@ function nv_ajax ( $endpoint, $callback )
 }
 
 
-wp_enqueue_style( "newamerykah-css", "/wp-content/plugins/newamerykah/style.css" );
 wp_enqueue_style( "nv-framework", "https://navalachy.cz/wp-content/themes/navalachy/assets/style.css" );
 wp_enqueue_style( "montserrat", "https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" );
+wp_enqueue_style( "newamerykah-css", "/wp-content/plugins/newamerykah/style.css" );
 ?>
