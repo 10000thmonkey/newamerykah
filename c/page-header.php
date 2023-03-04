0ed215ba8ed3d5fe771463b46a8a5e5a326b0ae9
happychildren <vojja01@gmail.com>
2023-03-04 08:59:01 +0100

    jkb

--- c/page-header.php
+++ c/page-header.php
@@ -13,7 +13,7 @@ nv_new_c (
 			$VAR["logo"] = "<img src='/wp-content/plugins/a/iosi-global-logo.svg'>";
 
 		return <<<HTML
-		<header class="site-header space-around-hg rows center">
+		<header class="site-header-iosi space-around-hg rows center">
 			{$VAR["logo"]}
 			<h1>{$VAR["h1"]}</h1>
 			<span>{$VAR["h2"]}</span>
