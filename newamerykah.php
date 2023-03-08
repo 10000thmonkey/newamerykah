cd7ed4c11b539cffc66a676e2d2d8b636b63cfc7
happychildren <vojja01@gmail.com>
2023-03-08 17:07:19 +0100

    edcec

--- newamerykah.php
+++ newamerykah.php
@@ -151,7 +151,7 @@ add_action( "after_setup_theme",
         if ( ! is_admin() )
         {
             add_action(
-                'wp_enqueue_scripts'.
+                'wp_enqueue_scripts',
                 function() {
                     wp_enqueue_style( "nv-framework", "https://navalachy.cz/wp-content/themes/navalachy/assets/framework.css" );
                     wp_enqueue_style( "montserrat", "https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700&display=swap" );
