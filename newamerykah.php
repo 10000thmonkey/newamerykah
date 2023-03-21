6142899f42d10140f4045d27f1165a575c74050f
happychildren <vojja01@gmail.com>
2023-03-21 15:42:44 +0100

    sdvsv

--- newamerykah.php
+++ newamerykah.php
@@ -224,9 +224,6 @@ function getIPAddress() {
 
 
 
-
-
-
 function disable_emojis() {
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
